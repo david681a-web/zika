68661271f004968c46a7d5c0e0dd977430928d83
David Eduardo de Jesus Ribeiro <David52428726@edu.df.senac.br>
2025-08-19 19:47:28 -0300

    e nois de novo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,8 @@
-INSERT INTO padaria (id, nome, preco) VALUES
-                                          (1, 'Pão Francês', 0.50),
-                                          (2, 'Croissant', 2.00),
-                                          (3, 'Baguete', 3.50),
-                                          (4, 'Pão de Queijo', 1.20),
-                                          (5, 'Sonho', 3.00),
-                                          (6, 'Pão Integral', 1.00),
-                                          (7, 'Ciabatta', 2.50),
-                                          (8, 'Rosca Doce', 1.80);
+INSERT INTO padaria (id, nome, preco) VALUES (1, 'Pão Francês', 0.50);
+INSERT INTO padaria (id, nome, preco) VALUES (2, 'Croissant', 2.00);
+INSERT INTO padaria (id, nome, preco) VALUES (3, 'Baguete', 3.50);
+INSERT INTO padaria (id, nome, preco) VALUES (4, 'Pão de Queijo', 1.20);
+INSERT INTO padaria (id, nome, preco) VALUES (5, 'Sonho', 3.00);
+INSERT INTO padaria (id, nome, preco) VALUES (6, 'Pão Integral', 1.00);
+INSERT INTO padaria (id, nome, preco) VALUES (7, 'Ciabatta', 2.50);
+INSERT INTO padaria (id, nome, preco) VALUES (8, 'Rosca Doce', 1.80);
